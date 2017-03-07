c5b0a38c3a10574009ba3eec7ae7a04560603153
trevormac <trevor.c.macgregor@gmail.com>
2017-03-07 13:44:10 -0500

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Trevor MacGregor on 2017-03-07.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
